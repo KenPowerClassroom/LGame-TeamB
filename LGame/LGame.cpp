e27f74d0f705fbf0bbb9dcbe4250009c951975a8
Ben Millar <c00236772@itcarlow.ie>
2020-01-08 14:14:55 +0000

    Drawing blank SFML window

--- LGame/LGame.cpp
+++ LGame/LGame.cpp
@@ -1,20 +1,29 @@
 // LGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1100,620,32U),"L Game",sf::Style::Default);
+
     std::cout << "Hello World!\n";
+
+	window.display();
+
+	system("PAUSE");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
