676c167b660fd0f2e6181a823996753a1cbafcf8
(Student) - Aaron O Neill <C00241596@itcarlow.ie>
2020-01-22 13:42:11 +0000

    Test files added

---
+++ LGameTest/Lpiece.unittest.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "..//LGame/LPiece.h"
+
+TEST(LPiece, LpieceTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
--- LGameTest/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "..//LGame/Board.h"
-
-TEST(Board, DefaultValue) {
-	Board mockBoard;
-  EXPECT_EQ(mockBoard.m_board[1][1], 3);
-  EXPECT_TRUE(true);
-}
