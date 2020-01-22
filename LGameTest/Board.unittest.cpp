7a58672f9efd4b0b0ae09362faf56827acd1c08b
(Student) - Aaron O Neill <C00241596@ITCARLOW.IE>
2020-01-22 13:48:30 +0000

    generic test written

--- LGameTest/Board.unittest.cpp
+++ LGameTest/Board.unittest.cpp
@@ -4,5 +4,8 @@
 TEST(Board, DefaultValueCheck) {
 	Board mockBoard;
 	EXPECT_EQ(mockBoard.m_board[1][1], 3);
-	EXPECT_TRUE(true);
+
+	EXPECT_EQ(mockBoard.m_board[3][1], 3);
+
+	EXPECT_EQ(mockBoard.m_board[2][0], 0);
 }
