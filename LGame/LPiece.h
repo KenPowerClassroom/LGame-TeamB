0f4bc9cbcf82a42ae15a6f178865ca034376fc92
(Student) - Michael Rainsford Ryan <c00239510@ITCARLOW.IE>
2020-01-22 14:51:29 +0000

    Added tests and data for LPiece preview

--- LGame/LPiece.h
+++ LGame/LPiece.h
@@ -6,8 +6,25 @@
 /// @Date 15/01/2020
 /// </summary>
 
+#include <array>
+
 class LPiece
 {
+public:
+	LPiece(int t_pieceColour, int t_rowOffset, int t_colOffset);
+
+	void setHidden(bool t_state);
+
+	std::array<std::array<int, 3>, 3> getShape();
+	int getRowOffset();
+	int getColOffset();
+	bool isHidden();
+
+private:
+	std::array<std::array<int, 3>, 3> m_shape;
+	int m_rowOffset;
+	int m_colOffset;
+	bool m_hidden;
 };
 
 #endif // !L_PIECE_H
--- LGameTest/Lpiece.unittest.cpp
+++ LGameTest/Lpiece.unittest.cpp
@@ -1,7 +1,26 @@
 #include "pch.h"
 #include "..//LGame/LPiece.h"
 
-TEST(LPiece, LpieceTest) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(LPiece, RedLpieceConstructer)
+{
+	LPiece lPiece(2, 0, 1);
+
+	ASSERT_EQ(2, lPiece.getShape()[0][0]);
+	ASSERT_EQ(2, lPiece.getShape()[0][1]);
+	ASSERT_EQ(2, lPiece.getShape()[1][1]);
+	ASSERT_EQ(2, lPiece.getShape()[2][1]);
+
+	ASSERT_EQ(true, lPiece.isHidden());
+
+	ASSERT_EQ(0, lPiece.getRowOffset());
+	ASSERT_EQ(1, lPiece.getColOffset());
+}
+
+TEST(LPiece, LpieceShowPiece)
+{
+	LPiece lPiece(2, 0, 1);
+
+	lPiece.setHidden(false);
+
+	ASSERT_EQ(false, lPiece.isHidden());
 }
