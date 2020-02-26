7f2a825c7e801ac61f34bc2ed2a1d33e0cd1cd92
Ben Millar <c00236772@itcarlow.ie>
2020-02-26 14:24:25 +0000

    Split responsibility of isMovePossible into two functions

--- LGame/MoveChecker.cpp
+++ LGame/MoveChecker.cpp
@@ -1,6 +1,7 @@
 #include "MoveChecker.h"
+#include <array>
 
-bool MoveChecker::isMovePossible(Board t_board, TileType t_currentPlayer)
+std::array<std::array<bool, 4>, 4> MoveChecker::getTruthBoard(Board t_board, TileType t_currentPlayer)
 {
 	for (auto row : t_board.m_board)
 	{
@@ -11,37 +12,31 @@ bool MoveChecker::isMovePossible(Board t_board, TileType t_currentPlayer)
 		}
 	}
 
+	// Simplify the board into empty (0) or non empty (1) cells
+	std::array<std::array<bool, 4>, 4> truthBoard;
+
+	for (int row = 0; row < 4; row++)
+	{
+		for (int col = 0; col < 4; col++)
+		{
+			// Set each cell to 1 if != empty, and 0 if == empty
+			truthBoard.at(row).at(col) = (t_board.m_board.at(row).at(col) != TileType::Empty);
+		}
+	}
+
+	return truthBoard;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+bool MoveChecker::isMovePossible(Board t_board, TileType t_currentPlayer)
+{
 	// Fail fast. Are there any blank sections that are 3 long?
 	for (auto row : t_board.m_board)
 	{
 		for (auto cell : row)
 		{
 		}
-	}
-	
-	// We check if the middle 'plus' of cells is occupied
-	int blockedCount{ 0 };
-
-	for (int row = 0; row < 4; row++)
-	{
-		for (int col = 1; col < 3; col++)
-		{
-			if (t_board.m_board.at(row).at(col) != TileType::Empty)
-			{
-				blockedCount++;
-			}
-
-			if (t_board.m_board.at(col).at(row) != TileType::Empty)
-			{
-				blockedCount++;
-			}
-		}
-	}
-
-	// If they're all occupied, it's not possible to move the piece
-	if (blockedCount >= 16)
-	{
-		return false;
 	}
 
 	return false;
--- LGame/MoveChecker.h
+++ LGame/MoveChecker.h
@@ -13,6 +13,20 @@ public:
 	MoveChecker() = default;
 	~MoveChecker() = default;
 
+	/// <summary>
+	/// @brief Generate a board of bools showing which cells are empty
+	/// </summary>
+	/// <param name="t_board">Game board to process</param>
+	/// <param name="t_currentPlayer">Player whose turn it is (to remove from board)</param>
+	/// <returns>4x4 array of bools; 0 if empty, 1 if not empty</returns>
+	static std::array<std::array<bool, 4>, 4> getTruthBoard(Board t_board, TileType t_currentPlayer = TileType::PlayerOne);
+
+	/// <summary>
+	/// @brief Determines if at least 1 move is possible for the current player
+	/// </summary>
+	/// <param name="t_board">Game board to process</param>
+	/// <param name="t_currentPlayer">Player whose turn it is (to remove from board)</param>
+	/// <returns>True if >=1 move is possible, false otherwise</returns>
 	static bool isMovePossible(Board t_board, TileType t_currentPlayer = TileType::PlayerOne);
 
 private:
